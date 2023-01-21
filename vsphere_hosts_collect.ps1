8274f8ad926bec3870bef35f0e3b545a67354648
Stanislav Musil <musil@sm-net.cz>
2023-01-21 14:47:33 +0100

    fix: disconnect from vcenter

--- vsphere_hosts_collect.ps1
+++ vsphere_hosts_collect.ps1
@@ -4,4 +4,4 @@ $VCENTER_SERVER="vCenter_FQDN_or_IP"
 
 connect-viserver -server $VCENTER_SERVER -User $VCENTER_USER -Password $VCENTER_PASSWORD
 get-vmhost|Export-csv vsphere_hosts.csv -NoTypeInformation
-
+disconnect-viserver
